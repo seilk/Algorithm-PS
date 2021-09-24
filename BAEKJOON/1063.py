7276ebd5c47457c6c69364ba7bb94925b14ab499
seilk <2018141123@yonsei.ac.kr>
2021-09-25 01:25:17 +0900

    (test) edit codeFrames

--- BAEKJOON/1063.py
+++ BAEKJOON/1063.py
@@ -1,35 +1,39 @@
 import sys
+
+
 def f():
     return map(str, sys.stdin.readline().split())
+
+
 N, K = f()
-N = int(N); K = int(K);
-lst = ['?' for i in range(N)] 
+N = int(N)
+K = int(K)
+lst = ['?' for i in range(N)]
 wheel = True
 for i in range(K):
-    c, x = f() 
+    c, x = f()
     c = int(c)
-    w = - (c % N) 
+    w = - (c % N)
     if i == 0:
-        lst[w] = x 
+        lst[w] = x
 
     elif x in lst:
-        if ((abs(lst.index(x)) + abs(w)) != N) : 
+        if ((abs(lst.index(x)) + abs(w)) != N):
             wheel = False
-            
+
         else:
             lst[w] = x
-            
 
     elif x not in lst:
-        if (lst[w] != '?') :
+        if (lst[w] != '?'):
             wheel = False
 
         else:
             lst[w] = x
 
     temp = []
-    for i in range(w, N + w, +1): 
-        temp.append(lst[i])   
+    for i in range(w, N + w, +1):
+        temp.append(lst[i])
     lst = temp
 
 if wheel:
@@ -37,3 +41,4 @@ if wheel:
         print(lst[i], end="")
 else:
     print("!")
+    ##
