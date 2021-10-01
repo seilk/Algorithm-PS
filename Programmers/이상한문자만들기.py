74d1ec2a3f67a3ead23f8c0d0f1e998a931b86f9
seilk <2018141123@yonsei.ac.kr>
2021-10-01 23:59:13 +0900

    Enumearate, lambda 공부

--- Programmers/이상한문자만들기.py
+++ Programmers/이상한문자만들기.py
@@ -18,3 +18,8 @@ print(solution(" ABCD abcd AbCd "))
 # http://ml-ko.kr/whirlwindtourpython/14-%EB%AC%B8%EC%9E%90%EC%97%B4%EA%B3%BC%20%EC%A0%95%EA%B7%9C%20%ED%91%9C%ED%98%84%EC%8B%9D.html
 
 # https://skgudwn34.tistory.com/10
+
+
+# Enumearate, lambda 공부
+def toWeirdCase(s):
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
