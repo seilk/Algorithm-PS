bee388e7d8bd02aa369edab2f649bdaab4a3f711
seilk <2018141123@yonsei.ac.kr>
2021-10-01 23:53:44 +0900

    BitMask 공부

--- BAEKJOON/13335_BitMask.py
+++ BAEKJOON/13335_BitMask.py
@@ -16,7 +16,7 @@ def f(a: bin, b: bin, o: str) -> bin:
         return(bin(int(a, 2) & int(b, 2)))
 
 
-x = bin(1 << brdL)  # 1(00)
+x = bin(1 << brdL)  # 1(00) #000
 t = 0
 bW = 0
 # 0b100
@@ -31,15 +31,22 @@ while tCheck != numT:
             bW -= bTrucks.popleft()
     if x[-1] == "0":
         if trucks and bW + trucks[0] <= maxW:
+            # 첫번째 칸으로 트럭을 이동시킨다
             bW += trucks[0]
             bTrucks.append(trucks.popleft())
             x = bin(int(x, 2) + int(bin(1), 2))
         else:
             pass
-
     t += 1
 print(t)
 
-# x = bin(1 << 3) #1000
-# y = bin(int(x, 2) ^ int(bin(1), 2)) #1000 vs 0001
-# print(y)
+x = bin(1 << 3)  # 1000
+y = bin(int(x, 2) ^ int(bin(1), 2))  # 1000 vs 0001
+print(y)
+
+########################################
+# x = bin(3)
+# y = 0b11
+# print(x == y)
+# # print(type(x))
+# print(bin(int(x, 2) + int(x, 2)))  # 111
