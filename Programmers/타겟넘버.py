a06594a103e883f7b8a992400dfecc3150e3136d
seilk <2018141123@yonsei.ac.kr>
2021-10-26 13:39:58 +0900

    Factorial이 아닌 DFS, 재귀는 언제나 Node로 생각할것

---
+++ Programmers/타겟넘버.py
@@ -0,0 +1,26 @@
+# n개의 음이 아닌 정수
+# 더하기 혹은 빼기
+# 숫자의 개수는 2~20개
+# 숫자는 고정시키고 덧셈 뺼셈만 조정해주면 됨
+# 시간복잡도 : 2^20
+ansSet = set()
+x = []
+
+
+def solution(numbers, target) -> int:
+    minus = [1, -1]
+
+    def DFS(idx, cnt):
+        if idx == len(numbers):
+            if sum(numbers) == target:
+                cnt += 1
+            return cnt
+        for i in range(2):
+            numbers[idx] *= minus[i]
+            cnt = DFS(idx + 1, cnt)
+            numbers[idx] *= minus[i]
+        return cnt
+    return DFS(0, 0)
+
+
+print(solution([1, 1, 1, 1, 1], 3))
