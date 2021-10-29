cbf37d309160a4c0992ad621e175a5a4771fa341
seilk <2018141123@yonsei.ac.kr>
2021-10-29 16:32:18 +0900

    DFS 문제풀이 + dp 문제풀이도 알아보기

---
+++ BAEKJOON/퇴사_14501.py
@@ -0,0 +1,26 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+info = [[0, 0]] + [list(map(int, input().split())) for i in range(n)]
+# 현재날짜 + [0] 이상의 날짜에서만 상담가능
+# 현재날짜 + [0]이 퇴사날짜이상이면 상담불가능
+maxVal = -1
+
+
+def DFS(day, n, val):
+    global maxVal
+    if day >= n + 1:
+        maxVal = max(maxVal, val)
+        return
+    for i in range(day, n + 1):
+        if i + info[i][0] <= n + 1:  # 상담을 마치는 날이 퇴사날짜면 상담 가능
+            DFS(i + info[i][0], n, val + info[i][1])
+        else:
+            DFS(day + 15, n, val)
+
+    return maxVal
+
+
+print(DFS(1, n, 0))
