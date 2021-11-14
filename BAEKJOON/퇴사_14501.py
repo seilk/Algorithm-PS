7d982a07311f22c31a57493d3c8e7bcc808224f1
seilk <2018141123@yonsei.ac.kr>
2021-11-15 02:58:24 +0900

    dfs

--- BAEKJOON/퇴사_14501.py
+++ BAEKJOON/퇴사_14501.py
@@ -10,17 +10,17 @@ maxVal = -1
 
 
 def DFS(day, n, val):
-    global maxVal
-    if day >= n + 1:
-        maxVal = max(maxVal, val)
-        return
-    for i in range(day, n + 1):
-        if i + info[i][0] <= n + 1:  # 상담을 마치는 날이 퇴사날짜면 상담 가능
-            DFS(i + info[i][0], n, val + info[i][1])
-        else:
-            DFS(day + 15, n, val)
+  global maxVal
+  if day >= n + 1:
+      maxVal = max(maxVal, val)
+      return
+  for i in range(day, n + 1):
+      if i + info[i][0] <= n + 1:  # 상담을 마치는 날이 퇴사날짜면 상담 가능
+          DFS(i + info[i][0], n, val + info[i][1])
+      else:
+          DFS(day + 15, n, val)
 
-    return maxVal
+  return maxVal
 
 
 print(DFS(1, n, 0))
