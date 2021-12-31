561298af2850a9de12eefd448946c8b1f7639c26
seilk <2018141123@yonsei.ac.kr>
2022-01-01 00:11:21 +0900

    백준 GoodBye 2021

---
+++ BAEKJOON/2021은 무엇이 특별할까?.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def isPrime(n):
+  if n == 1:
+    return False
+  if n == 2:
+    return True
+  elif n > 2:
+    for i in range(2, int(n ** 0.5) + 1):
+      if n % i == 0:
+        return False
+    return True
+
+
+def find(tg):
+  for i in range(len(prime) - 1):
+    n = prime[i] * prime[i + 1]
+    if tg < n:
+      return n
+
+
+if __name__ == "__main__":
+  input = sys.stdin.readline
+  num = int(input().rstrip())
+  prime = []
+  for k in range(104):
+    if isPrime(k):
+      prime.append(k)
+  print(find(num))
