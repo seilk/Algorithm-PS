d2ca680f1fafe7ddecfc7a5986738114703c1751
seilk <2018141123@yonsei.ac.kr>
2022-01-11 22:16:46 +0900

    최단경로는 BFS

---
+++ BAEKJOON/미로 탐색_2178.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10 ** 4)
+input = sys.stdin.readline
+N, M = map(int, input().split())
+ground = [list(map(int, list(input().rstrip()))) for i in range(N)]
+visited = [[0] * M for i in range(N)]
+
+
+def BFS(x, y):
+  queue = deque([(x, y)])
+  while queue:
+    x, y = queue.popleft()
+    for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+      if 0 <= nx < N and 0 <= ny < M:
+        if ground[nx][ny] == 1:
+          if not visited[nx][ny]:
+            visited[nx][ny] = visited[x][y] + 1
+            queue.append((nx, ny))
+
+
+visited[0][0] = 1
+BFS(0, 0)
+ans = visited[N - 1][M - 1]
+print(ans) if ans else print(-1)
