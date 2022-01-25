44491925ff495006b1ffb1e777161bfaaf2ad820
seilk <2018141123@yonsei.ac.kr>
2022-01-25 21:34:43 +0900

    2차원 DFS, 백트래킹, 중복 제거, AC

--- BAEKJOON/넴모넴모 (Easy)_14712.py
+++ BAEKJOON/넴모넴모 (Easy)_14712.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def canBreak(i, j):
+def canBreak(i, j): # 왼->오 , 위->아래 방향으로 탐색할거라서 위쪽으로 만들어질 수 있는 사각형만 파악하면 됨.
   return visited[i - 1][j - 1] == 1 and visited[i - 1][j] == 1 and visited[i][j - 1] == 1
 
 
@@ -11,9 +11,9 @@ def CHECK(i, j):
     cnt += 1
     return
   cnt += 1
-  for r in range(i, N + 1):
-    jj = j if r == i else 1  # 중복 제거
-    for c in range(jj, M + 1):
+  for r in range(i, N + 1): # 2차원 백트래킹 중복 제거 인덱스 값 조정 : i부터~
+    jj = j if r == i else 1  # 같은 행일때는 j부터 해야하고 더 아래쪽 행일때는 1부터 탐색
+    for c in range(jj, M + 1): # 1또는 j부터~
       if not canBreak(r, c):
         visited[r][c] = 1
         CHECK(r, c + 1)
