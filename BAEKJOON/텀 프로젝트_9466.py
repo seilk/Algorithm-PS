543bae7f0bc59a9716ba692deec647139f591903
seilk <2018141123@yonsei.ac.kr>
2022-02-03 18:53:45 +0900

    유니온 파인드 같은 DFS

---
+++ BAEKJOON/텀 프로젝트_9466.py
@@ -0,0 +1,45 @@
+import sys
+sys.setrecursionlimit(10**6)
+In = lambda : sys.stdin.readline().rstrip()
+MIS = lambda : map(int, In().split())
+def findTeam(node, N):
+  global included, arr, visited, endd
+  visited[node]=1
+  if included[arr[node]]!=0: # 어떤 그룹에 이미 포함되어 있거나 그룹을 이루지 못한 경우
+    visited[node]=0
+    included[node]=-1
+    return False
+  if arr[node] == node: # 자기자신
+    included[node]=1
+    return False
+  if visited[arr[node]]: # 현재 노드가 가리키는 노드가 이미 중간에 선택된 노드라면
+    included[node]=1
+    endd = arr[node]
+    return True
+  if findTeam(arr[node], N):
+    visited[node] = 0
+    if node == endd:
+      included[node] = 1
+      return False
+    included[node] = 1
+    return True
+  visited[node]=0
+  included[node]=-1
+  return False
+
+def solve():
+  global arr, included, visited, endd
+  N=int(In())
+  arr=[-1]+[*MIS()]
+  included=[0]*(N+1)
+  visited=[0]*(N+1)
+  for node in range(1, N+1):
+    if not included[node]:
+      endd = 0
+      findTeam(node,N)
+  print(included[1:].count(-1))
+
+if __name__ == "__main__":
+  T = int(In())
+  for t in range(T):
+    solve()
