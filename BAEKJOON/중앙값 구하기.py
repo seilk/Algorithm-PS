ec5eceebf257dc504bfc2e69232bba290f64b7d0
seilk <2018141123@yonsei.ac.kr>
2022-02-27 13:52:35 +0900

    우선순위 큐 응용

--- BAEKJOON/중앙값 구하기.py
+++ BAEKJOON/중앙값 구하기.py
@@ -4,52 +4,125 @@ from heapq import heappush, heappop
 In = lambda : sys.stdin.readline().rstrip()
 MIS = lambda : map(int, In().split())
 T = int(In())
+
+
+def getArray():
+  arr=[0]
+  for d in range(M // 10):
+    arr += [*MIS()]
+  arr += [*MIS()]
+  return arr
+
+
 for t in range(T):
   M = int(In())
-  MAX = 1<<32+1
+  MAX = 1<<32 + 1
   left = [MAX]
   right = [MAX]
-  arr=[0]
-  for d in range(M//10):
-    arr+=[*MIS()]
-  arr+=[*MIS()]
+  ll,rr=0,0
+  arr = getArray()
   ans = [arr[1]]
   mid = arr[1]
   for m in range(2,M+1):
     if m&1:
-      if arr[m]<=-left[0]:
-        heappush(left, -arr[m])
-        heappush(right, mid)
-        mid = -heappop(left)
-      elif -left[0]<arr[m]<=mid:
+      if arr[m]<=-left[0]: # 구간 1
+        if ll<rr:
+          heappush(left, -arr[m])
+          ll+=1
+        else:
+          heappush(left, -arr[m])
+          heappush(right, mid)
+          rr+=1
+          mid = -heappop(left)
+
+      elif -left[0]<arr[m]<=mid: # 구간 2
         if -left[0]==-MAX:
           heappush(left, -arr[m])
+          ll+=1
         else:
-          heappush(left, -mid)
-          mid = arr[m]
-      elif mid<arr[m]<=right[0]:
+          if ll < rr:
+            heappush(left, -arr[m])
+            ll+=1
+          else:
+            heappush(right, mid)
+            mid = arr[m]
+            rr+=1
+
+      elif mid<arr[m]<=right[0]: # 구간 3
         if right[0]==MAX:
           heappush(right, arr[m])
+          rr+=1
+        else:
+          if ll>rr:
+            heappush(right,arr[m])
+            rr+=1
+          else:
+            heappush(left, -mid)
+            ll+=1
+            mid = arr[m]
+
+      elif right[0]<arr[m]: # 구간 4
+        if rr<ll:
+          heappush(right,arr[m])
+          rr+=1
         else:
           heappush(left, -mid)
-          mid = arr[m]
-      elif right[0]<arr[m]:
-        heappush(left, -mid)
-        heappush(right, arr[m])
-        mid = heappop(right)
+          ll+=1
+          heappush(right, arr[m])
+          mid = heappop(right)
       ans.append(mid)
-    else:
+
+    else: #짝수 일 때
       if arr[m] < mid:
         heappush(left, -arr[m])
+        ll+=1
       else:
         heappush(right, arr[m])
-  print(M//2+1) if M%2 else print(M//2)
-  div=M//10
+        rr+=1
+
+  print((M//2)+1)
+  div=((M//2)+1)//10
   if div==0:
     print(*ans)
   else:
     a=0
     for p in range(1,div+1):
-      print(*ans[a:10*p])
+      print(*ans[a:(10*p)])
       a=10*p
-    print(*ans[a:])
+    if a < M//2+1:
+      print(*ans[a:])
+
+# 1
+# 31
+# 1 2 3 4 5 6 7 8 9 10
+# 11 12 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26 27 28 29 30
+# 31
+
+# 1
+# 11
+# -1 -2 -3 -4 -5 -6 -7 -8 -9 -10
+# 100
+
+# 1
+# 5
+# -2147483648 2147483648 -2147483648 -2147483648 -2147483648
+
+# 1
+# 19
+# 1 2 3 4 5 6 7 8 9 10
+# 11 12 13 14 15 16 17 18 19
+
+# 3
+# 21
+# 1 2 3 4 5 6 7 8 9 10
+# 11 12 13 14 15 16 17 18 19
+# 20 21
+# 31
+# 1 2 3 4 5 6 7 8 9 10
+# 11 12 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26 27 28 29 30
+# 31
+# 19
+# 1 2 3 4 5 6 7 8 9 10
+# 11 12 13 14 15 16 17 18 19
