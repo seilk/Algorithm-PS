3297174a4637c8acdeb3424071173dcd14f89a1c
seilk <2018141123@yonsei.ac.kr>
2022-03-04 00:40:28 +0900

    에라토스테네스의 체, 숏코딩

--- BAEKJOON/소수 구하기_1929.py
+++ BAEKJOON/소수 구하기_1929.py
@@ -1,13 +1,5 @@
-import sys
-N, M = map(int, sys.stdin.readline().rstrip().split())
-print = sys.stdout.write
-arr = [0] * (M+1)
-arr[1] = 1
-for i in range(2, M+1):
-	for j in range(2, M//i+1): # 자기 자신은 제외하고 배수를 지워줌
-		arr[i*j] = 1
-
-for i in range(N, M+1):
-	if arr[i] == 0:
-		print("%s" % i + "\n")
-		
+m,n=map(int,input().split())
+*l,=range(n+1)
+for i in l[2:]:
+	if l[i]>=m:print(i)
+	l[i::i]=n//i*[0] # i 부터 끝까지 i 간격으로 https://blog.wonkyunglee.io/3
