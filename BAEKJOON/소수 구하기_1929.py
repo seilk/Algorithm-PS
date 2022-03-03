2b6cb2d24f1f04f73a074eb4a70cf7a97aea1fa8
seilk <2018141123@yonsei.ac.kr>
2022-03-04 00:46:44 +0900

    에라토스테네스의 체, 숏코딩

--- BAEKJOON/소수 구하기_1929.py
+++ BAEKJOON/소수 구하기_1929.py
@@ -2,4 +2,6 @@ m,n=map(int,input().split())
 *l,=range(n+1)
 for i in l[2:]:
 	if l[i]>=m:print(i)
-	l[i::i]=n//i*[0] # i 부터 끝까지 i 간격으로 https://blog.wonkyunglee.io/3
+	l[i::i]=n//i*[0]
+	# i 부터 끝까지 i 간격으로 https://blog.wonkyunglee.io/3
+	# 슬라이싱을 통해 새로운 배열을 할당하면 원본 배열에서 슬라이싱 한 부분만 새로운 배열로 바뀜
