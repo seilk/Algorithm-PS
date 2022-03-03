30f00db1fd2ccdc75ec9e1eb484058c18c638310
seilk <2018141123@yonsei.ac.kr>
2022-03-03 09:46:10 +0900

    유니온 파인드, 부모노드 개수 구해줄 때 find()

---
+++ BAEKJOON/Count Circle Groups_10216_UF.py
@@ -0,0 +1,56 @@
+import sys
+
+In = lambda: sys.stdin.readline().rstrip()
+MIS = lambda: map(int, In().split())
+
+
+def find(x, arr):
+	if arr[x] != x:
+		arr[x] = find(arr[x], arr)
+	return arr[x]
+
+
+def union(x, y, arr):
+	a = find(x, arr)
+	b = find(y, arr)
+	if a < b:
+		arr[b] = arr[a]
+	else:
+		arr[a] = arr[b]
+
+
+def dist(x1, y1, x2, y2):
+	return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+
+def solve():
+	N = int(In())
+	xx = [0] * N
+	yy = [0] * N
+	rr = [0] * N
+	
+	for n in range(N):
+		xx[n], yy[n], rr[n] = MIS()
+	
+	arr = [i for i in range(N)]
+	# ans = 0
+	for n in range(N):
+		for m in range(n + 1, N):
+			# math 라이브러리 사용하면 시간초과. 두 점 사이 거리 구할 때 sqrt 사용 자제
+			if dist(xx[n], yy[n], xx[m], yy[m]) <= (rr[n] + rr[m]) ** 2:
+				if find(n, arr) == find(m, arr): continue
+				union(n, m, arr)
+				
+	tot = 0 # 부모노드 개수 갱신
+	vist = [0] * N
+	for n in range(N):
+		# arr의 값으로 찾으면 안됨, arr의 값의 find로 찾아야함, 아직 부모노드가 갱신이 안된 지점이 있을 수 있음.
+		if vist[find(arr[n], arr)] == 0:
+			vist[arr[n]] = 1
+			tot += 1
+	print(tot)
+
+if __name__ == "__main__":
+	T = int(In())
+	for t in range(T):
+		solve()
