bc84797291c68dcc5464e2eaf7a1c0a8d48f247d
seilk <2018141123@yonsei.ac.kr>
2022-06-05 21:37:48 +0900

    삼성, 구현

--- BAEKJOON/주사위 굴리기.py
+++ BAEKJOON/주사위 굴리기.py
@@ -14,11 +14,6 @@ def roll(pre, cmd) :
 	       [pre[0], pre[2], pre[6], pre[3], pre[4], pre[1], pre[5]]]  # 남
 	return nxt[cmd-1]
 
-n, m, x, y, k = MIS()  # 세로길이, 가로길이, 시작x, 시작y, 명령 횟수
-
-brd = [[*MIS()] for i in range(n)]
-curr = [-1, 0, 0, 0, 0, 0, 0]  # 현재 주사위 상태
-
 def changeVal(curr, brd, nx, ny) :
 	"""
 	이동한 칸에 쓰여 있는 수가 0이면, 주사위의 바닥면에 쓰여 있는 수가 칸에 복사된다.
@@ -31,12 +26,20 @@ def changeVal(curr, brd, nx, ny) :
 		brd[nx][ny] = 0
 	return curr, brd
 
-for cmd in [*MIS()] :
-	dx, dy = moving(cmd)
-	nx, ny = x+dx, y+dy
-	if nx < 0 or nx >= n or ny < 0 or ny >= m :
-		continue
-	curr = roll(curr, cmd)
-	curr, brd = changeVal(curr, brd, nx, ny)
-	x, y = nx, ny
-	print(curr[1])
+def solve() :
+	n, m, x, y, k = MIS()  # 세로길이, 가로길이, 시작x, 시작y, 명령 횟수
+	brd = [[*MIS()] for i in range(n)]
+	curr = [-1, 0, 0, 0, 0, 0, 0]  # 현재 주사위 상태
+
+	for cmd in [*MIS()] :
+		dx, dy = moving(cmd)
+		nx, ny = x+dx, y+dy
+		if nx < 0 or nx >= n or ny < 0 or ny >= m :
+			continue
+		curr = roll(curr, cmd) # 주사위가 범위를 초과하면 굴러가서도 안됨.
+		curr, brd = changeVal(curr, brd, nx, ny)
+		x, y = nx, ny
+		print(curr[1])
+
+if __name__ == "__main__" :
+	solve()
